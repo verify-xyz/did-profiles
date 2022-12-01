4aa11a93599d0d7569c41e756f28283efc1150af
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2022-12-01 15:40:27 +0100

    Initial-Setup - Expect string as @Param

--- backend/src/ipfs-api/ipfs-api.controller.ts
+++ backend/src/ipfs-api/ipfs-api.controller.ts
@@ -6,8 +6,8 @@ export class IpfsApiController {
     constructor(private readonly ipfsApiService: IpfsApiService) {}
 
     @Get('add/:text')
-    async addToIpfs(@Param() text: any) {
-        const result = await this.ipfsApiService.addStringToIpfs(text.text);
+    async addToIpfs(@Param('text') text: string): Promise<string> {
+        const result = await this.ipfsApiService.addStringToIpfs(text);
 
         const response = { hash: result };
         const json = JSON.stringify(response);
@@ -16,10 +16,10 @@ export class IpfsApiController {
     }
 
     @Get('read/:hash')
-    async readFromIpfs(@Param() hash: any) {
-        const result = await this.ipfsApiService.getStringFromIpfs(hash.hash);
+    async readFromIpfs(@Param('hash') hash: string): Promise<string> {
+        const result = await this.ipfsApiService.getStringFromIpfs(hash);
 
-        const response = { message: result };
+        const response = { text: result };
         const json = JSON.stringify(response);
 
         return json;
