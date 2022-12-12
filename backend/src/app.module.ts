80312481c8ee0c2854d56c583df8986f1fe5ffd9
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2022-12-12 18:19:17 +0100

    client sign module removed at backend side

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,22 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { IpfsApiModule } from './ipfs-api/ipfs-api.module';
-import { CryptionModule } from './cryption/cryption.module';
 import { LitService } from './lit/lit.service';
 import { CryptionService } from './cryption/cryption.service';
 import { IpfsApiService } from './ipfs-api/ipfs-api.service';
 import { HttpModule } from '@nestjs/axios';
 import { RegisterModule } from './register/register.module';
-import { RegisterController } from './register/register.controller';
-import { RegisterService } from './register/register.service';
-import { ClientSignModule } from './client-sign/client-sign.module';
-import { ClientSignController } from './client-sign/client-sign.controller';
-import { ClientSignService } from './client-sign/client-sign.service';
 
 @Module({
     imports: [ConfigModule.forRoot(), HttpModule, RegisterModule],
     controllers: [AppController],
-    providers: [AppService, LitService, CryptionService, IpfsApiService, ClientSignService],
+    providers: [AppService, LitService, CryptionService, IpfsApiService],
 })
 export class AppModule {}
