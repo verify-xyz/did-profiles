9e40b700014bc85f7dc46c61ce1e1132318d051e
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2022-12-14 16:56:47 +0100

    Unused code deleted

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,8 +7,6 @@ import { CryptionService } from './cryption/cryption.service';
 import { IpfsApiService } from './ipfs-api/ipfs-api.service';
 import { HttpModule } from '@nestjs/axios';
 import { RegisterModule } from './register/register.module';
-import { DidResolverController } from './did-resolver/did-resolver.controller';
-import { DidResolverService } from './did-resolver/did-resolver.service';
 import { DidResolverModule } from './did-resolver/did-resolver.module';
 
 @Module({
