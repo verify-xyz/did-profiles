5fbc4e805f428c173c804592e593847c1a031787
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2022-12-27 13:00:42 +0100

    Toggle button state fixed

--- frontend/src/components/toggleButton.js
+++ frontend/src/components/toggleButton.js
@@ -1,9 +1,15 @@
-import React, { /* useState, */ useEffect } from 'react';
+import React, { useState /* useEffect */ } from 'react';
 import '../styles/styleToggleButton.css';
 
 // eslint-disable-next-line react/prop-types
 export const ToggleButton = ({ setValue }) => {
-    let value = 'private';
+    const [isToggled, setIsToggled] = useState(false);
+    const onToggle = () => {
+        setIsToggled(!isToggled);
+        setValue(!isToggled);
+    };
+
+    /* let value = 'private';
 
     useEffect(() => {
 
@@ -16,13 +22,13 @@ export const ToggleButton = ({ setValue }) => {
             value = 'private';
         }
         setValue(value);
-    }
+    } */
 
     return (
         <div className='toggleButtonMainContainer' >
             <label>Private</label>
             <label className="switch">
-                <input type="checkbox" onClick={checkboxClickedHandler}></input>
+                <input type="checkbox" onChange={onToggle} checked={isToggled}></input>
                 <span className="slider round"></span>
             </label>
             <label>Public</label>
