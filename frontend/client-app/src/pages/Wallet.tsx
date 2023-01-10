8a196cd265cf37f1b913316d02e0af38edc40d3f
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2023-01-10 12:01:29 +0100

    Personal sign

--- frontend/client-app/src/pages/Wallet.tsx
+++ frontend/client-app/src/pages/Wallet.tsx
@@ -1,17 +1,30 @@
 import { useWeb3React } from "@web3-react/core"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { injected } from "../utils/connector";
+import { MetaMaskInpageProvider } from '@metamask/providers';
+import { Buffer } from 'buffer';
+// import { Buffer } from 'node:buffer';
+
+/*declare global {
+    interface Window {
+        ethereum?: MetaMaskInpageProvider
+    }
+}*/
+
+declare var window: any;
 
 export default function Wallet() {
     const { active, account, library, connector, activate, deactivate } = useWeb3React();
+    // const ethereum = window.ethereum as MetaMaskInpageProvider;
+    const [personalSignResult, setPersonalSignResult] = useState('Not signed');
 
     async function connect() {
         try {
             console.log(injected);
             await activate(injected);
 
-            if (active) { 
-                console.log('account: ' + account); 
+            if (active) {
+                console.log('account: ' + account);
             } else {
                 console.log('NOT CONNECTED');
             }
@@ -31,8 +44,26 @@ export default function Wallet() {
         }
     }
 
+    async function personalSign() {
+        console.log('personal sign');
+
+        const exampleMessage = 'Example `personal_sign` message';
+        try {
+            const from = account;
+            const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
+            const sign = await window.ethereum.request({
+                method: 'personal_sign',
+                params: [msg, from, 'Example password'],
+            });
+            setPersonalSignResult(sign);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     useEffect(() => {
         const connectWalletOnPageLoad = async () => {
+            console.log('Connect wallet on page load');
             if (localStorage?.getItem('isWalletConnected') === 'true') {
                 try {
                     await activate(injected)
@@ -50,6 +81,8 @@ export default function Wallet() {
             <div onClick={connect} className="Wallet-button">Connect to MetaMask</div>
             {active ? <span className="Wallet-connection">Connected with <b>{account}</b></span> : <span className="Wallet-connection">Not connected</span>}
             <div onClick={disconnect} className="Wallet-button">Disconnect</div>
+            <div onClick={personalSign} className="Wallet-button Wallet-button-personal-sign">Personal Sign</div>
+            <span className='Wallet-personal-sign-result'><label>Personal sign result:</label> {personalSignResult}</span>
         </div>
     )
 }
