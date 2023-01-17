86b279d170b13d1559568d12600c8f6a2f9a663f
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2023-01-17 15:55:03 +0100

    Update Manage.tsx

--- frontend/client-app/src/pages/Manage.tsx
+++ frontend/client-app/src/pages/Manage.tsx
@@ -1,10 +1,8 @@
 import { EthrDID } from "ethr-did";
 import { Web3Provider } from "@ethersproject/providers";
 import { useState } from "react";
-import { LitAuthSig, useLitAuthSig } from "../hooks/useLitAuthSig";
 
 export default function Manage() {
-    const { authSig, personalSign, account } = useLitAuthSig();
     const [currentAccount, setCurrentAccount] = useState('None');
 
     async function buttonChangeOwnershipClickedHandler() {
@@ -12,16 +10,11 @@ export default function Manage() {
     }
 
     async function changeOwnership() {
-        const provider = new Web3Provider((window as any).ethereum);
+        setCurrentAccount('None');
 
+        const provider = new Web3Provider((window as any).ethereum);
         const chainNameOrId = await (window as any).ethereum.request({ method: 'eth_chainId' });
         const accounts = await (window as any).ethereum.request({ method: 'eth_accounts' });
-        /*const accounts = await (window as any).ethereum.request({
-            method: 'eth_requestAccounts',
-        });*/
-
-        console.log('accounts------------------------');
-        console.log(accounts);
 
         const ethrDid = new EthrDID({
             identifier: accounts[0],
@@ -29,13 +22,7 @@ export default function Manage() {
             provider: provider
         });
 
-        console.log('EthrDID-------------------------------------');
-        console.log(ethrDid);
-
         const newOwner = await ethrDid.changeOwner(accounts[0]);
-
-        console.log('new owner------------------------');
-        console.log(newOwner);
 
         setCurrentAccount(newOwner);
     }
@@ -43,13 +30,7 @@ export default function Manage() {
     return (
         <div className='Manage-main-container'>
             <div onClick={buttonChangeOwnershipClickedHandler} className="Manage-button">Change ownership</div>
-            <label className="Manage-label">Current account: <b>{currentAccount}</b></label>
-
-            {/* <div className='Manage-grid-container01'>
-                <label className="Manage-label">Message:</label>
-                <input className="Manage-input" id='messageID'></input>
-                <div className="Manage-button" id="mabnageID" onClick={buttonManageClickedHandler}>Manage</div>
-            </div> */}
+            <label className="Manage-label">Transaction Hash: <b>{currentAccount}</b></label>
         </div>
     );
 };
